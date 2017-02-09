66a9d1d8bf544141f57b9cd2f189e35453b85b20
ariel0058 <ariel0058@hotmail.com>
2017-02-09 10:01:13 -0800

    update

--- ZenithWebSite/Migrations/EventMigrations/Configuration.cs
+++ ZenithWebSite/Migrations/EventMigrations/Configuration.cs
@@ -19,53 +19,15 @@ namespace ZenithWebSite.Migrations.EventMigrations
             
         }
 
-        //protected override void Seed(ZenithWebSite.Models.ApplicationDbContext context)
-        //{
-        //    context.Activity.AddOrUpdate(a => a.activityId, getActivities());
-        //    context.SaveChanges();
-
-        //    context.Event.AddOrUpdate(e => e.eventId, getEvents());
-        //    context.SaveChanges();
-        //}
-
-        //private Event[] getEvents()
-        //{
-        //    var events = new List<Event>
-        //    {
-        //       new Event() { eventFrom=new DateTime(2017,2,1,15,30,00), eventTo=new DateTime(2017,2,1,16,30,00), createEvent=new DateTime(2017,1,18), isActive=true, activityId=1  },
-        //       new Event() { eventFrom=new DateTime(2017,2,4,12,30,00), eventTo=new DateTime(2017,2,4,13,30,00), createEvent=new DateTime(2017,1,12), isActive=true, activityId=2 },
-        //       new Event() { eventFrom=new DateTime(2017,2,5,11,30,00), eventTo=new DateTime(2017,2,5,14,30,00),  createEvent=new DateTime(2017,1,30), isActive=false, activityId=3  },
-        //       new Event() { eventFrom=new DateTime(2017,2,5,18,30,00), eventTo=new DateTime(2017,2,5,20,30,00), createEvent=new DateTime(2017,1,14), isActive=false, activityId=4  },
-        //       new Event() { eventFrom=new DateTime(2017,2,6,15,30,00), eventTo=new DateTime(2017,2,6,16,30,00), createEvent=new DateTime(2017,1,18), isActive=true, activityId=5  },
-        //       new Event() { eventFrom=new DateTime(2017,2,7,15,30,00), eventTo=new DateTime(2017,2,7,16,30,00), createEvent=new DateTime(2016,12,17), isActive=false, activityId=6 },
-        //       new Event() { eventFrom=new DateTime(2017,2,8,15,30,00), eventTo=new DateTime(2017,2,8,16,30,00), createEvent=new DateTime(2017,1,10), isActive=true, activityId=4  },
-        //       new Event() { eventFrom=new DateTime(2017,2,10,10,30,00), eventTo=new DateTime(2017,2,10,14,30,00), createEvent=new DateTime(2017,1,6), isActive=false, activityId=5  },
-        //       new Event() { eventFrom=new DateTime(2017,2,12,10,30,00), eventTo=new DateTime(2017,2,12,14,30,00), createEvent=new DateTime(2017,1,6), isActive=false, activityId=6  },
-        //       new Event() { eventFrom=new DateTime(2017,2,14,10,30,00), eventTo=new DateTime(2017,2,14,14,30,00), createEvent=new DateTime(2017,1,6), isActive=false, activityId=2  },
-
-
-        //    };
-        //    return events.ToArray();
-        //}
-
-        //private Activity[] getActivities()
-        //{
-        //    var activities = new List<Activity>
-        //    {
-        //       new Activity() { description="Seniors  Golf Tournament", createActivity=new DateTime(2016,12,6) },
-        //       new Activity() { description="Leadership General Assembly Meeting", createActivity=new DateTime(2016,12,3) },
-        //       new Activity() { description="Youth Bowling Tournament", createActivity=new DateTime(2016,11,20) },
-        //       new Activity() { description="Young ladies cooking lessons", createActivity=new DateTime(2016,10,4) },
-        //       new Activity() { description="Pancake Breakfast", createActivity=new DateTime(2016,10,17) },
-        //       new Activity() { description="Swimming Exercise for parents", createActivity=new DateTime(2016,11,25) },
-        //    };
-        //    return activities.ToArray();
-        //}
-
-
-
-         protected override void Seed(ZenithWebSite.Models.ApplicationDbContext context)
+        protected override void Seed(ZenithWebSite.Models.ApplicationDbContext context)
         {
+            context.Activity.AddOrUpdate(a => a.activityId, getActivities());
+            context.SaveChanges();
+
+            context.Event.AddOrUpdate(e => e.eventId, getEvents());
+            context.SaveChanges();
+
+            //add admin and member
             var roleManager = new RoleManager<IdentityRole>(new RoleStore<IdentityRole>(context));
             if (!roleManager.RoleExists("Admin"))
                 roleManager.Create(new IdentityRole("Admin"));
@@ -100,9 +62,42 @@ namespace ZenithWebSite.Migrations.EventMigrations
                 if (result.Succeeded)
                     userManager.AddToRole(userManager.FindByName(user.UserName).Id, "Member");
             }
+
         }
 
+        private Event[] getEvents()
+        {
+            var events = new List<Event>
+            {
+               new Event() { eventFrom=new DateTime(2017,2,1,15,30,00), eventTo=new DateTime(2017,2,1,16,30,00), createEvent=new DateTime(2017,1,18), isActive=true, activityId=1  },
+               new Event() { eventFrom=new DateTime(2017,2,4,12,30,00), eventTo=new DateTime(2017,2,4,13,30,00), createEvent=new DateTime(2017,1,12), isActive=true, activityId=2 },
+               new Event() { eventFrom=new DateTime(2017,2,5,11,30,00), eventTo=new DateTime(2017,2,5,14,30,00),  createEvent=new DateTime(2017,1,30), isActive=false, activityId=3  },
+               new Event() { eventFrom=new DateTime(2017,2,5,18,30,00), eventTo=new DateTime(2017,2,5,20,30,00), createEvent=new DateTime(2017,1,14), isActive=false, activityId=4  },
+               new Event() { eventFrom=new DateTime(2017,2,6,15,30,00), eventTo=new DateTime(2017,2,6,16,30,00), createEvent=new DateTime(2017,1,18), isActive=true, activityId=5  },
+               new Event() { eventFrom=new DateTime(2017,2,7,15,30,00), eventTo=new DateTime(2017,2,7,16,30,00), createEvent=new DateTime(2016,12,17), isActive=false, activityId=6 },
+               new Event() { eventFrom=new DateTime(2017,2,8,15,30,00), eventTo=new DateTime(2017,2,8,16,30,00), createEvent=new DateTime(2017,1,10), isActive=true, activityId=4  },
+               new Event() { eventFrom=new DateTime(2017,2,10,10,30,00), eventTo=new DateTime(2017,2,10,14,30,00), createEvent=new DateTime(2017,1,6), isActive=false, activityId=5  },
+               new Event() { eventFrom=new DateTime(2017,2,12,10,30,00), eventTo=new DateTime(2017,2,12,14,30,00), createEvent=new DateTime(2017,1,6), isActive=false, activityId=6  },
+               new Event() { eventFrom=new DateTime(2017,2,14,10,30,00), eventTo=new DateTime(2017,2,14,14,30,00), createEvent=new DateTime(2017,1,6), isActive=false, activityId=2  },
 
+
+            };
+            return events.ToArray();
+        }
+
+        private Activity[] getActivities()
+        {
+            var activities = new List<Activity>
+            {
+               new Activity() { description="Seniors  Golf Tournament", createActivity=new DateTime(2016,12,6) },
+               new Activity() { description="Leadership General Assembly Meeting", createActivity=new DateTime(2016,12,3) },
+               new Activity() { description="Youth Bowling Tournament", createActivity=new DateTime(2016,11,20) },
+               new Activity() { description="Young ladies cooking lessons", createActivity=new DateTime(2016,10,4) },
+               new Activity() { description="Pancake Breakfast", createActivity=new DateTime(2016,10,17) },
+               new Activity() { description="Swimming Exercise for parents", createActivity=new DateTime(2016,11,25) },
+            };
+            return activities.ToArray();
+        }
 
     }
 }
